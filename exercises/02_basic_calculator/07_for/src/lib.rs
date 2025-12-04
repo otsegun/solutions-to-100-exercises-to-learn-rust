3e32e5b351016d37a8901b4e7310e355eedaaad1
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2025-12-04 15:10:33 +0100

    Complete stion 2: 02_basic_calculator exercises

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,12 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
     
-    let mut product = 1;
+    let mut result = 1;
 
     for i in 1..=n {
-        product *= i;
+        result *= i;
     }
-    product
+    result
     
 }
 
