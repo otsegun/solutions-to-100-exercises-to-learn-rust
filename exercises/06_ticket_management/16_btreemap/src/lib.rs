70764e50447cda88b909c42878c9eb5f27ace7f7
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2025-12-19 05:44:45 +0100

    Add solutions for 06_ticket_management/16_btreemap

--- exercises/06_ticket_management/16_btreemap/src/lib.rs
+++ exercises/06_ticket_management/16_btreemap/src/lib.rs
@@ -3,7 +3,9 @@
 //  references to the tickets, ordered by their `TicketId`.
 //  Implement additional traits on `TicketId` if needed.
 
+use std::collections::btree_map::{Iter, Values};
 use std::collections::BTreeMap;
+use std::iter::Map;
 use std::ops::{Index, IndexMut};
 use ticket_fields::{TicketDescription, TicketTitle};
 
@@ -13,7 +15,7 @@ pub struct TicketStore {
     counter: u64,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct TicketId(u64);
 
 #[derive(Clone, Debug, PartialEq)]
@@ -40,7 +42,7 @@ pub enum Status {
 impl TicketStore {
     pub fn new() -> Self {
         Self {
-            tickets: todo!(),
+            tickets: BTreeMap::new(),
             counter: 0,
         }
     }
@@ -54,18 +56,37 @@ impl TicketStore {
             description: ticket.description,
             status: Status::ToDo,
         };
-        todo!();
+        self.tickets.insert(id, ticket);
         id
     }
 
     pub fn get(&self, id: TicketId) -> Option<&Ticket> {
-        todo!()
+        self.tickets.get(&id)
     }
 
     pub fn get_mut(&mut self, id: TicketId) -> Option<&mut Ticket> {
-        todo!()
+        self.tickets.get_mut(&id)
     }
 }
+
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = Values<'a, TicketId, Ticket>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.values()
+    }
+}
+
+// impl<'a> IntoIterator for &'a TicketStore {
+//     type Item = &'a Ticket;
+
+//     type IntoIter = Map<Iter<'a, TicketId, Ticket>, fn((&'a TicketId, &'a Ticket)) -> &'a Ticket>;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         // We use a function pointer (|(_, v)| v) to keep the type nameable.
+//         self.tickets.iter().map(|(_id, ticket)| ticket)
+//     }
+// }
 
 impl Index<TicketId> for TicketStore {
     type Output = Ticket;
