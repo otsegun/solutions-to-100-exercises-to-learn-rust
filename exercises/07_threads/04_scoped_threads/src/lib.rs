70a23bbef33f0acb45d8bf166bccdc3177bee018
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-01-08 19:21:23 +0100

    Add solution for 07_threads/06_interior_mutability

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -2,8 +2,38 @@
 //  and compute the sum of each half in a separate thread.
 //  Don't perform any heap allocation. Don't leak any memory.
 
+use std::thread::{Scope, ScopedJoinHandle};
+pub fn sum2(v: Vec<i32>) -> i32 {
+    let mid = v.len() / 2;
+    let (left, right) = v.split_at(mid);
+
+    std::thread::scope(|s| {
+        let left = s.spawn(|| left.iter().sum::<i32>());
+        let right = s.spawn(|| right.iter().sum::<i32>());
+        left.join().unwrap() + right.join().unwrap()
+    })
+}
+
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let length: usize = v.len();
+    let midpoint: usize = length /2;
+
+
+    std::thread::scope(|scope|{
+        let first_handle: ScopedJoinHandle<'_, i32> = scope.spawn(||{
+            let first = &v[..midpoint]; 
+            first.iter().sum()
+            
+        });
+
+        let second_handle: ScopedJoinHandle<'_, i32> = scope.spawn(||{
+            let second = &v[midpoint..];
+           second.iter().sum()
+        });      
+
+        first_handle.join().unwrap() + second_handle.join().unwrap()
+    })
+    
 }
 
 #[cfg(test)]
